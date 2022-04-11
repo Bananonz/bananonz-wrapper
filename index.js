2d5883a27e058ee3ff997c9edc6c948d2ba16648
Bananonz <soggiumattia@gmail.com>
2022-04-11 19:56:13 +0200

    test: 1.0.32

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ async function request(endpoint, input='') {
   return res;
 }
 
-module.exports.chatbot = async function(x, ownername, botname) {
+module.exports.chatbot = async function(x, ownername, botname, uid) {
     if(!x) throw new Error("The field 'content' is empty - chatbot.")
     if(!ownername) throw new Error("The field 'ownername' is empty - chatbot.")
     if(!botname) throw new Error("The field 'botname' is empty - chatbot.")
-    const res = await fetch(`${baseurl}chatbot?msg=${encodeURIComponent(x)}&owner=${encodeURIComponent(ownername)}&botname=${encodeURIComponent(botname)}`)
+    if(!uid) uid = "usernp" 
+    const res = await fetch(`${baseurl}chatbot?msg=${encodeURIComponent(x)}&owner=${encodeURIComponent(ownername)}&botname=${encodeURIComponent(botname)}&uid${encodeURIComponent(uid)}`)
     const resp = await res.json()
     return resp.response;
 }
@@ -31,6 +32,13 @@ module.exports.password = async function() {
 module.exports.pet = async function(avatar) {
   if(!avatar) throw new Error('The field avatar is empty - pet.')
   const res = await fetch(`${baseurl}pet?image=${avatar}`)
+  const response = `${baseurl}drake?${input}`
+  return (response);
+}
+
+module.exports.reverse = async function(text) {
+  if(!text) throw new Error('The field text is empty - reverse.')
+  const res = await fetch(`${baseurl}reverse?text=${text}`)
   const json = await res.json()
-  return json.password;
+  return json.reverse;
 }
